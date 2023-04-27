678cfdff5e7d7a58b6038e1ba56ecd2ef14e2fb0
Santi Legaristi Royo <slegaris@c1r12s4.42madrid.com>
2023-04-27 09:32:01 +0200

    reduced blob

--- ft_otp.py
+++ ft_otp.py
@@ -6,7 +6,7 @@
 #    By: slegaris <slegaris@student.42madrid.com>   +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/04/26 01:11:18 by slegaris          #+#    #+#              #
-#    Updated: 2023/04/26 01:26:15 by slegaris         ###   ########.fr        #
+#    Updated: 2023/04/26 16:04:32 by slegaris         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -31,7 +31,9 @@ def generate_key(input_file, output_file):
         sys.exit(1)
 
     encrypted_key = hashlib.sha256(key.encode('utf-8')).hexdigest()
+    # print(f"Key: {encrypted_key}")
     checksum = hashlib.md5(encrypted_key.encode('utf-8')).hexdigest()
+    # print(f"checksum: {checksum}")
 
     try:
         with open(output_file, 'w') as f:
@@ -43,14 +45,34 @@ def generate_key(input_file, output_file):
 
 def verify_checksum(key, checksum):
     calculated_checksum = hashlib.md5(key.encode('utf-8')).hexdigest()
+    # print(f"calculated_checksum: {calculated_checksum}")
     return calculated_checksum == checksum
 
-def get_otp(key, time_interval=60):
+def get_otp(key, time_interval=30):
     current_time = int(time.time() // time_interval)
+    # print(f"Time: {current_time}")
     hasher = hashlib.sha256()
-    hasher.update(key.encode('utf-8'))
+    # print(f"hasher: {hasher}")
     hasher.update(str(current_time).encode('utf-8'))
-    return hasher.hexdigest()[:6]
+    hex_value = hasher.hexdigest()[:6]
+    # print(f"hashed output: {hex_value}")
+    six_digit_number = int(hex_value, 16) % 1000000
+    return six_digit_number
+
+def print_colored(text, color):
+    color_codes = {
+        'black': '30',
+        'red': '31',
+        'green': '32',
+        'yellow': '33',
+        'blue': '34',
+        'magenta': '35',
+        'cyan': '36',
+        'white': '37',
+        'reset': '0'
+    }
+
+    return f"\033[{color_codes[color]}m{text}\033[0m"
 
 def main():
     parser = argparse.ArgumentParser(description="Generate a one-time password.")
@@ -81,7 +103,7 @@ def main():
             sys.exit(1)
 
         otp = get_otp(key)
-        print(otp)
+        print(f"{print_colored('Your one time password:', 'red')} {print_colored(otp, 'cyan')}")
         sys.exit(0)
 
     parser.print_help()
